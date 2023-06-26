663ef1c7ad0422a5b050445c5d1ed4d3c65d9e75
junnyJun <chbe5082@naver.com>
2023-06-26 14:13:54 +0900

    upset config, api1

--- eureka/src/main/kotlin/git/io/eureka/EurekaApplication.kt
+++ eureka/src/main/kotlin/git/io/eureka/EurekaApplication.kt
@@ -2,8 +2,10 @@ package git.io.eureka
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class EurekaApplication
 
 fun main(args: Array<String>) {
