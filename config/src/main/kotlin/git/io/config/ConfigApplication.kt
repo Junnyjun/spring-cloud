f7709c5ef657ebe10407d0a4ddeb374e37f0c88b
JunnyJun <chbe5082@naver.com>
2023-06-29 01:34:27 +0900

    yml update

--- config/src/main/kotlin/git/io/config/ConfigApplication.kt
+++ config/src/main/kotlin/git/io/config/ConfigApplication.kt
@@ -3,8 +3,10 @@ package git.io.config
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 class ConfigApplication
 
